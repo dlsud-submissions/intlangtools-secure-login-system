2ad7a5334666627efaa94cdff26bcefe419a68f2
cH0NKIIs34L <chonkyseal.work@gmail.com>
2026-07-15 13:22:11 +0800

    feat: add responsive styling and modern UI for login system

--- src/views/login.view.php
+++ src/views/login.view.php
@@ -7,6 +7,14 @@
 
     <title><?= APP_NAME ?></title>
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+        rel="stylesheet"
+    >
     <link rel="stylesheet" href="src/views/assets/css/style.css">
 </head>
 
